53ba62ae0c30e52f3c5f22aa7d2579a045fd1be8
kvpratama <kvpratama@gmail.com>
2021-08-26 10:26:29 +0900

    convert mhd to nii

--- data/preprocessing/mhd2nii.py
+++ data/preprocessing/mhd2nii.py
@@ -1,8 +1,13 @@
 import SimpleITK as sitk
 import numpy as np
+import glob
 
-img = sitk.GetArrayFromImage(sitk.ReadImage("CaseLspine2.mhd"))
-img = np.transpose(img, axes=[1,2,0])
+mhd_files = glob.glob("./img/*.mhd")
+
+for mhd_file in mhd_files:
+	img = sitk.GetArrayFromImage(sitk.ReadImage(mhd_file))
+	img = np.transpose(img, axes=[1,2,0])
 
 
-sitk.WriteImage(sitk.GetImageFromArray(img), "CaseLspine2.nii")
+	sitk.WriteImage(sitk.GetImageFromArray(img), mhd_file[:-3] + 'nii')
+	print(mhd_file)
